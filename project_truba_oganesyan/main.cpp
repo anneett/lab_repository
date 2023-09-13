e7b639e305f29a106034e367829c388c8d33fc7d
anneett <ann_oganesyan59@mail.ru>
2023-09-13 22:26:20 +0300

    lab1_vvod-vivod
    Проблема с переменной типа bool: если ввести 0, то считывает дальше, если НЕ 0 - то проблема дальше

--- project_truba_oganesyan/main.cpp
+++ project_truba_oganesyan/main.cpp
@@ -15,21 +15,68 @@ struct KS
 {
 	string nazvan;
 	int tsekh;
-	int tsekh_pab;
+	int tsekh_rab;
 	char effect;
+};
+
+Truba CreateTruba()
+{
+	Truba tb;
+	cout << "Enter pipe elevation: ";
+	cin >> tb.otmetka;
+	cout << "Enter pipe length: ";
+	cin >> tb.dlina;
+	cout << "Enter the pipe attribute: ";
+	cin >> tb.remont;
+	return tb;
+};
+
+KS CreateKS()
+{
+	KS kc;
+	cout << "Enter the title: ";
+	cin >> kc.nazvan;
+	cout << "Enter the number of workshops: ";
+	cin >> kc.tsekh;
+	cout << "Enter the number of workshops in operation: ";
+	cin >> kc.tsekh_rab;
+	cout << "Enter efficiency: ";
+	cin >> kc.effect;
+	return kc;
+};
+
+void PrintTruba(Truba tr)
+{
+	cout << "Pipe elevation: " << tr.otmetka
+		<< "\tPipe length: " << tr.dlina
+		<< "\tThe pipe attribute: " << tr.remont << endl;
+};
+
+void PrintKS(KS ks)
+{
+	cout << "Title: " << ks.nazvan
+		<< "\tEnter the number of workshops: " << ks.tsekh
+		<< "\tEnter the number of workshops in operation: " << ks.tsekh_rab
+		<< "\tEnter efficiency: " << ks.effect << endl;
 };
 
 int main()
 {
-	vector <Truba> trub_group;
+	/*vector <Truba> trub_group;
 	size_t n = 0;
 	cin >> n;
-	trub_group.resize(n);
+	trub_group.resize(n);*/
 
-	vector <KS> ks_group;
+	Truba tr = CreateTruba();
+	PrintTruba(tr);
+
+	/*vector <KS> ks_group;
 	size_t m = 0;
 	cin >> m;
-	ks_group.resize(m);
+	ks_group.resize(m);*/
+
+	KS ks = CreateKS();
+	PrintKS(ks);
 
 	return 0;
 }
