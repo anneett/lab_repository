ab43b7d63bfd1fb01a767637c3b140838f32c45b
anneett <ann_oganesyan59@mail.ru>
2023-09-21 15:14:30 +0300

    Fixed
    Бесконечный цикл, все записывает

--- project_truba_oganesyan/main.cpp
+++ project_truba_oganesyan/main.cpp
@@ -5,18 +5,18 @@ using namespace std;
 
 struct Truba
 {
-	string otmetka;
-	double dlina;
-	int diametr;
-	bool remont;
+	string otmetka = "0 km";
+	double dlina = 0;
+	int diametr = 0;
+	bool remont = false;
 };
 
 struct KS
 {
-	string nazvan; 
-	int tsekh;
-	int tsekh_rab;
-	char effect;
+	string nazvan = "no";
+	int tsekh = 0;
+	int tsekh_rab = 0;
+	char effect = 'A';
 };
 
 Truba CreateTruba()
@@ -26,6 +26,8 @@ Truba CreateTruba()
 	cin >> tb.otmetka;
 	cout << "Enter pipe length: ";
 	cin >> tb.dlina;
+	cout << "Enter pipe diameter: ";
+	cin >> tb.diametr;
 	cout << "Enter the pipe attribute: ";
 	cin >> tb.remont;
 	return tb;
@@ -48,16 +50,17 @@ KS CreateKS()
 void PrintTruba(Truba tr)
 {
 	cout << "Pipe elevation: " << tr.otmetka
-		<< "\tPipe length: " << tr.dlina
-		<< "\tThe pipe attribute: " << tr.remont << endl;
+		<< "\nPipe length: " << tr.dlina
+		<< "\nPipe diameter: " << tr.diametr
+		<< "\nThe pipe attribute: " << tr.remont << endl;
 };
 
 void PrintKS(KS ks)
 {
 	cout << "Title: " << ks.nazvan
-		<< "\tEnter the number of workshops: " << ks.tsekh
-		<< "\tEnter the number of workshops in operation: " << ks.tsekh_rab
-		<< "\tEnter efficiency: " << ks.effect << endl;
+		<< "\nEnter the number of workshops: " << ks.tsekh
+		<< "\nEnter the number of workshops in operation: " << ks.tsekh_rab
+		<< "\nEnter efficiency: " << ks.effect << endl;
 };
 
 //int selection()
@@ -77,84 +80,79 @@ void PrintKS(KS ks)
 
 int main()
 {
-	cout << "Select menu item: "
-		<< "\n1. Add pipe;"
-		<< "\n2. Add KS;"
-		<< "\n3. View all objects;"
-		<< "\n4. Edit pipe;"
-		<< "\n5. Edit KS;"
-		<< "\n6. Save;"
-		<< "\n7. Download;"
-		<< "\n8. Exit." << endl;
+	Truba pipe;
+	KS station;
+	while (true) {
+		cout << "Select menu item: "
+			<< "\n1. Add pipe;"
+			<< "\n2. Add KS;"
+			<< "\n3. View all objects;"
+			<< "\n4. Edit pipe;"
+			<< "\n5. Edit KS;"
+			<< "\n6. Save;"
+			<< "\n7. Download;"
+			<< "\n8. Exit." << endl;
 
-	int number = 0;
-	cout << "Select: ";
-	cin >> number;
-	while (cin.fail())
-	{
-		cin.clear();
-		cin.ignore(1000, '\n');
-		cout << "Try again\n";
+		int number = 0;
+		cout << "Select: ";
 		cin >> number;
-	}
+		while (cin.fail())
+		{
+			cin.clear();
+			cin.ignore(1000, '\n');
+			cout << "Try again\n";
+			cin >> number;
+		}
 
-	switch (number)
-	{
-	case 1:
-	{
-		Truba tr = CreateTruba();
-		//PrintTruba(tr);
-		main();
-		break;
+		switch (number)
+		{
+		case 1:
+		{
+			pipe = CreateTruba();
+			break;
+		}
+		case 2:
+		{
+			station = CreateKS();
+			break;
+		}
+		case 3:
+		{
+			cout << "vibrano 3" << endl;
+			PrintTruba(pipe);
+			PrintKS(station);
+			break;
+		}
+		case 4:
+		{
+			cout << "vibrano 4" << endl;
+			break;
+		}
+		case 5:
+		{
+			cout << "vibrano 5" << endl;
+			break;
+		}
+		case 6:
+		{
+			cout << "vibrano 6" << endl;
+			break;
+		}
+		case 7:
+		{
+			cout << "vibrano 7" << endl;
+			break;
+		}
+		case 8:
+		{
+			cout << "vixod" << endl;
+			break;
+		}
+		default:
+		{
+			cout << "Enter a number from 1 to 8!" << endl;
+			break;
+		}
+		}
 	}
-	case 2:
-	{
-		KS ks = CreateKS();
-		//PrintKS(ks);
-		main();
-		break;
-	}
-	case 3:
-	{
-		cout << "vibrano 3" << endl;
-		main();
-		break;
-	}
-	case 4:
-	{
-		cout << "vibrano 4" << endl;
-		main();
-		break;
-	}
-	case 5:
-	{
-		cout << "vibrano 5" << endl;
-		main();
-		break;
-	}
-	case 6:
-	{
-		cout << "vibrano 6" << endl;
-		main();
-		break;
-	}
-	case 7:
-	{
-		cout << "vibrano 7" << endl;
-		main();
-		break;
-	}
-	case 8:
-	{
-		cout << "vixod" << endl;
-		break;
-	}
-	default:
-	{
-		cout << "Enter a number from 1 to 8!" << endl;
-		break;
-	}
-	}
-
-	return 0;
 }
