12d2c0e301148fab2fbf637c5074e483c6676c1c
anneett <ann_oganesyan59@mail.ru>
2023-12-04 20:14:45 +0300

    next_commit

--- project_truba_oganesyan/Truba_and_CS.h
+++ project_truba_oganesyan/Truba_and_CS.h
@@ -11,7 +11,7 @@ public:
 	void DownloadPipe(unordered_map <int, Truba>& pipes);
 	int Filter_pipes(unordered_map <int, Truba>& tb);
 
-	void Add_CS(unordered_map <int, CS>& pipes);
+	void Add_CS(unordered_map <int, CS>& cs);
 	void OutputCS(unordered_map <int, CS>& cs);
 	void DownloadCS(unordered_map <int, CS>& cs);
 	int Filter_CS(unordered_map <int, CS>& cs);
